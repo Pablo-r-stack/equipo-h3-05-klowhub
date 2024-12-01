7ad944ec5b6218523b393e716207cc21098a006c
Alekosescu <alekosescu@gmail.com>
2024-12-01 18:34:09 -0300

    Arreglado el nombre de userRoutes.js

--- Klowhub-backend/src/index.js
+++ Klowhub-backend/src/index.js
@@ -9,7 +9,7 @@ import "./config/passport.js"; // Configuración de Passport
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import registerRoutes from "./routes/register.routes.js";
-import userRoute from "./routes/user.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import setupSwagger from "./swagger.js";
 import claseRoutes from "./routes/claseRoutes.js";
 
